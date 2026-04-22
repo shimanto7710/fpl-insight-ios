797d3e5b51fd9d55c3c164dd49ec82c1f86d5b9e
Shimanto <shimanto7710@gmail.com>
2026-04-23 01:55:10 +0200

    Initial Commit

---
+++ FPL Insight/FPL_InsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FPL_InsightApp.swift
+//  FPL Insight
+//
+//  Created by Shimanto A. on 23/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FPL_InsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FPL InsightTests/FPL_InsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  FPL_InsightTests.swift
+//  FPL InsightTests
+//
+//  Created by Shimanto A. on 23/4/26.
+//
+
+import Testing
+@testable import FPL_Insight
+
+struct FPL_InsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
